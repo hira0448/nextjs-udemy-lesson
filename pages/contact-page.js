b59868d4fe2e48927f6adc00bcbde1b67384d9aa
hirano <rhi691508@gmail.com>
2022-02-20 14:16:48 +0900

    Imageコンポーネントを使用したAvatar画像の表示

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,7 +1,25 @@
 import Layout from "../components/Layout";
+import Image from "next/image";
 
 const Contact = () => {
-  return <Layout title="Contact">Contact page</Layout>;
+  return (
+    <Layout title="Contact">
+      <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+        <div className="mt-4">
+          <div className="font-bold">Contact info</div>
+          <div className="flex justify-center mt-4">
+            <Image
+              className="rounded-full"
+              src="/avatar.jpg"
+              width={60}
+              height={60}
+              alt="Avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Contact;
