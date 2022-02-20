6b9488250c855bb45796bedf104b30091ff65d38
hirano <rhi691508@gmail.com>
2022-02-20 14:21:48 +0900

    表示させたAvatar画像にEmail、Phone項目を表示

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -16,6 +16,14 @@ const Contact = () => {
               alt="Avatar"
             />
           </div>
+          <div className="mt-4">
+            <p className="font-bold">Address</p>
+            <p className="text-xs mt-2 text-gray-600" city A></p>
+            <p className="font-bold mt-3">Email</p>
+            <p className="text-xs mt-2 text-gray-600">abc@gmail.com</p>
+            <p className="font-bold mt-3">Phone</p>
+            <p className="text-xs mt-2 text-gray-600">000-123-456</p>
+          </div>
         </div>
       </div>
     </Layout>
