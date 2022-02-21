dfa91527809bd86c5f4683e0e85e633167713f7a
hirano <rhi691508@gmail.com>
2022-02-22 06:35:41 +0900

    取得するデータがある場合とない場合の表示をif文で切り替える処理

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,6 +2,23 @@ import Link from "next/link";
 import Layout from "../../components/Layout";
 import { getAllPostIds, getAllPostsData, getPostData } from "../../lib/posts";
 
+export default function Post({ post }) {
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Layout title={post.title}>
+      <p className="m-4">
+        {"ID : "}
+        {post.id}
+      </p>
+      <p className="mb-8 text-xl font-bold">{post.title}</p>
+      <p className="px-10">{post.body}</p>
+    </Layout>
+  );
+}
+
 export async function getStaticPaths() {
   const paths = await getAllPostIds();
 
