bdb4f23760486c94182e63c2b52cb0cd58699a66
hirano <rhi691508@gmail.com>
2022-02-22 06:31:20 +0900

    paramsで受け取ったidをpropsのpost で返す処理

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Layout from "../../components/Layout";
-import { getAllPostIds, getPostData } from "../../lib/posts";
+import { getAllPostIds, getAllPostsData, getPostData } from "../../lib/posts";
 
 export async function getStaticPaths() {
   const paths = await getAllPostIds();
@@ -9,5 +9,15 @@ export async function getStaticPaths() {
     paths,
     // 100件を超えるデータを取得する際に100以降のデータを404で返す処理
     fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { post: post } = await getAllPostsData(params.id);
+
+  return {
+    props: {
+      post,
+    },
   };
 }
