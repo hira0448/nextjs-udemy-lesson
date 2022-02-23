c70cb174dce6a0e208d96d1c7932cc96cbf68f9d
hirano <rhi691508@gmail.com>
2022-02-23 18:20:11 +0900

    詳細ページにアイコンを追加

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/link-passhref */
 import Link from "next/link";
 import Layout from "../../components/Layout";
 import { getAllPostIds, getAllPostsData, getPostData } from "../../lib/posts";
@@ -15,6 +16,25 @@ export default function Post({ post }) {
       </p>
       <p className="mb-8 text-xl font-bold">{post.title}</p>
       <p className="px-10">{post.body}</p>
+      <Link href="/blog-page">
+        <div className="flex cursor-pointer mt-12">
+          <svg
+            className="w-6 h-6 mr-3"
+            fillRule="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+            ></path>
+          </svg>
+          <span>Back to blog-page</span>
+        </div>
+      </Link>
     </Layout>
   );
 }
